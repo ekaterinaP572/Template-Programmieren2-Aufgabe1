759a7ac7f16a259d2a79f1249814cd82801cae65
Ekaterina Popelikhova <ekaterina.popelikhova@student.htw-berlin.de>
2026-05-05 08:54:54 +0200

    not really important

--- src/main/org/htw/prog2/aufgabe1/Main.java
+++ src/main/org/htw/prog2/aufgabe1/Main.java
@@ -2,7 +2,7 @@ package org.htw.prog2.aufgabe1;
 
 public class Main {
     public static void main(String[] args){
-        SeqFile p = new SeqFile("data/protease_reference.fasta");
+        SeqFile p = new SeqFile("data/protease_sequences.fasta");
         System.out.println(p.getNumberOfSequences());
     }
 }
